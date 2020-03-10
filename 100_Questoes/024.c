e9e415bf90f52b4c9c1c69debab87182dc178b28
Maria Barros <48946383+44Mozart@users.noreply.github.com>
2020-03-10 16:00:10 +0000

    Create 024.c

---
+++ 100_Questoes/024.c
@@ -0,0 +1,10 @@
+int remRep (char texto []) {
+    int i, j = 0;
+    for (i = 0; texto[i] != '\0'; i++){
+        if (texto[i] != texto[i+1]){
+            texto[j] = texto[i];
+            j++;
+        }
+    }
+    texto[j] = '\0';
+    r
